a52bfe367ca4a07c9f59a53d652c6fa8a361da1e
Ozgur Ozerdem (Temp) <oozerdem@qti.qualcomm.com>
2025-07-30 10:56:09 -0700

    try

--- deneme.py
+++ deneme.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-# Generate sample data
+# Generate sample 
 dates = pd.date_range(start="2023-01-01", periods=10)
 values = np.random.randn(10).cumsum()
 data = pd.DataFrame({'Date': dates, 'Value': values})
